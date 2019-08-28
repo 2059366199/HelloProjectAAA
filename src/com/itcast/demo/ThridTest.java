21f85e7897a87f106a848b1af84fe617857b8ae8
Administrator <2059366199@qq.com>
2019-08-28 23:01:28 +0800

    第三次上传，修改了一些代码

--- src/com/itcast/demo/ThridTest.java
+++ src/com/itcast/demo/ThridTest.java
@@ -3,5 +3,8 @@ package com.itcast.demo;
 public class ThridTest {
     public static void main(String[] args) {
         System.out.println("第三次上传的是一个java文件！！！");
+        System.out.println("新增代码");
+        System.out.println("-------------------------------");
+        System.out.println("新增结束！！！");
     }
 }
